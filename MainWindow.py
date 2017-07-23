2f52d0e4ee767e38d3770b6aa9fd4d6f94092fe9
Unknown <jack_n_whittle@hotmail.com>
2017-07-23 22:08:00 +0100

    Fix to plotting. Added session splitting, time filtering

--- MainWindow.py
+++ MainWindow.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from pubsub import pub
+from pydispatch import dispatcher
 
 
 class Plotter(object):
@@ -21,14 +22,18 @@ class Plotter(object):
         self.line, = self.ax.plot([1, 2, 3], [1, 2, 3])
         self.ax.set_xlabel('X label')
         self.ax.set_ylabel('Y label')
+        plt.tight_layout()
         pub.subscribe(self.change_x_listener, 'UpdateX')
         pub.subscribe(self.replot_listener, 'UpdatePlot')
+        dispatcher.connect(self.replot_listener, signal="UpdatePlot", sender=dispatcher.Any)
 
     def replot_listener(self, plot_params):
         self.ax.clear()
         self.ax.set_xlabel(plot_params['x_name'])
         self.ax.set_ylabel(plot_params['y_name'])
-        self.ax.plot(plot_params['x_data'], plot_params['y_data'])
+        self.ax.plot(list(plot_params['x_data']), list(plot_params['y_data']))
+        self.ax.grid('on')
+        self.fig.canvas.draw()
 
     def change_x_listener(self, update):
         self.ax.set_xlabel(update)
@@ -37,7 +42,6 @@ class Plotter(object):
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(716, 631)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
@@ -46,45 +50,51 @@ class Ui_MainWindow(object):
         self.formLayout.setFieldGrowthPolicy(QtWidgets.QFormLayout.AllNonFixedFieldsGrow)
         self.formLayout.setLabelAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.formLayout.setObjectName("formLayout")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setObjectName("label")
-        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label)
-        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox.setObjectName("comboBox")
-        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.comboBox)
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setObjectName("label_2")
-        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_2)
-        self.comboBox_2 = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox_2.setObjectName("comboBox_2")
-        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.comboBox_2)
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setObjectName("label_3")
-        self.formLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.label_3)
-        self.timeEdit = QtWidgets.QTimeEdit(self.centralwidget)
-        self.timeEdit.setObjectName("timeEdit")
-        self.formLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.timeEdit)
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setObjectName("label_4")
-        self.formLayout.setWidget(4, QtWidgets.QFormLayout.LabelRole, self.label_4)
-        self.timeEdit_2 = QtWidgets.QTimeEdit(self.centralwidget)
-        self.timeEdit_2.setObjectName("timeEdit_2")
-        self.formLayout.setWidget(4, QtWidgets.QFormLayout.FieldRole, self.timeEdit_2)
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setObjectName("pushButton")
-        self.formLayout.setWidget(5, QtWidgets.QFormLayout.FieldRole, self.pushButton)
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName("pushButton_2")
         self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.pushButton_2)
+
+        self.label_session = QtWidgets.QLabel(self.centralwidget)
+        self.label_session.setObjectName("label_session")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_session)
+        self.comboBox_session = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox_session.setObjectName("comboBox_session")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.comboBox_session)
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setObjectName("comboBox")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.comboBox)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.comboBox_2 = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox_2.setObjectName("comboBox_2")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.comboBox_2)
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setObjectName("label_3")
+        self.formLayout.setWidget(4, QtWidgets.QFormLayout.LabelRole, self.label_3)
+        self.timeEdit = QtWidgets.QTimeEdit(self.centralwidget)
+        self.timeEdit.setObjectName("timeEdit")
+        self.formLayout.setWidget(4, QtWidgets.QFormLayout.FieldRole, self.timeEdit)
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setObjectName("label_4")
+        self.formLayout.setWidget(5, QtWidgets.QFormLayout.LabelRole, self.label_4)
+        self.timeEdit_2 = QtWidgets.QTimeEdit(self.centralwidget)
+        self.timeEdit_2.setObjectName("timeEdit_2")
+        self.formLayout.setWidget(5, QtWidgets.QFormLayout.FieldRole, self.timeEdit_2)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.formLayout.setWidget(6, QtWidgets.QFormLayout.FieldRole, self.pushButton)
         self.horizontalLayout.addLayout(self.formLayout)
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.horizontalLayout.addItem(spacerItem)
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
 
-        # self.figure = plt.figure()
         self.plotter = Plotter()
-
         self.canvas = FigureCanvas(self.plotter.fig)
         self.canvas.draw()
         self.toolbar = NavigationToolbar(self.canvas, self.centralwidget)
@@ -114,4 +124,24 @@ class Ui_MainWindow(object):
         self.label_4.setText(_translate("MainWindow", "End Time:"))
         self.pushButton.setText(_translate("MainWindow", "Apply"))
         self.pushButton_2.setText(_translate("MainWindow", "Load Data"))
+        self.label_session.setText(_translate("MainWindow", "Session:"))
+
+        dispatcher.connect(self.update_x_combo, signal="UpdateXCombo", sender=dispatcher.Any)
+        dispatcher.connect(self.update_y_combo, signal="UpdateYCombo", sender=dispatcher.Any)
+        dispatcher.connect(self.update_time_fields, signal="UpdateTimeFields", sender=dispatcher.Any)
+
+    def update_x_combo(self, update):
+        self.comboBox.setCurrentText(update)
+
+    def update_y_combo(self, update):
+        self.comboBox_2.setCurrentText(update)
+
+    def update_time_fields(self, s_time, e_time):
+        self.timeEdit.setMinimumTime(s_time)
+        self.timeEdit.setMaximumTime(e_time)
+        self.timeEdit.setTime(s_time)
+        self.timeEdit_2.setMinimumTime(s_time)
+        self.timeEdit_2.setMaximumTime(e_time)
+        self.timeEdit_2.setTime(e_time)
+
 
--- StartChartingApp.py
+++ StartChartingApp.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 import MainWindow
 import pandas as pd
 from pubsub import pub
-
+from pydispatch import dispatcher
 
 class StartQT4(QMainWindow):
     def __init__(self, parent=None):
         QDialog.__init__(self, parent)
+        self.showMaximized()
         self.ui = MainWindow.Ui_MainWindow()
         # attributes initialised in subsequent methods
         self.dm = None
@@ -18,6 +19,8 @@ class StartQT4(QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.choose_file)
         self.ui.comboBox.currentIndexChanged.connect(self.x_changed)
         self.ui.comboBox_2.currentIndexChanged.connect(self.y_changed)
+        self.ui.comboBox_session.currentIndexChanged.connect(self.session_changed)
+        self.ui.pushButton.clicked.connect(self.apply_btn_press)
 
     @pyqtSlot()
     def choose_file(self):
@@ -29,18 +32,35 @@ class StartQT4(QMainWindow):
             self.dm = DataManager(raw_data)
             self.ui.comboBox.addItems(self.dm.x_columns)
             self.ui.comboBox_2.addItems(self.dm.y_columns)
-            pub.sendMessage('UpdatePlot', plot_params=self.dm.plot_data)
+            self.ui.comboBox_session.addItems([str(x) for x in self.dm.get_sorted_session_keys()])
+            dispatcher.send('UpdateXCombo', update=self.dm.x_columns[0])
+            # pub.sendMessage('UpdatePlot', plot_params=self.dm.plot_data)
+            # dispatcher.send("UpdatePlot", plot_params=self.dm.plot_data)
+
+    def apply_btn_press(self):
+        start_time = self.ui.timeEdit.time().toPyTime()
+        end_time = self.ui.timeEdit_2.time().toPyTime()
+        self.dm.apply_time_filter(start_time, end_time)
+        dispatcher.send("UpdatePlot", plot_params=self.dm.plot_data)
 
     @pyqtSlot()
     def x_changed(self):
         new_value = self.ui.comboBox.currentText()
         self.dm.update_x_plot_data(new_value)
-        pub.sendMessage('UpdatePlot', plot_params=self.dm.plot_data)
+        dispatcher.send("UpdatePlot", plot_params=self.dm.plot_data)
+        # pub.sendMessage('UpdatePlot', plot_params=self.dm.plot_data)
 
     def y_changed(self):
         new_value = self.ui.comboBox_2.currentText()
         self.dm.update_y_plot_data(new_value)
-        pub.sendMessage('UpdatePlot', plot_params=self.dm.plot_data)
+        dispatcher.send("UpdatePlot", plot_params=self.dm.plot_data)
+        # pub.sendMessage('UpdatePlot', plot_params=self.dm.plot_data)
+
+    def session_changed(self):
+        new_session_key = pd.to_datetime(self.ui.comboBox_session.currentText(), format='%Y/%m/%d %H:%M:%S')
+        self.dm.update_session_plot_data(new_session_key)
+        dispatcher.send("UpdatePlot", plot_params=self.dm.plot_data)
+        dispatcher.send("UpdateTimeFields", s_time=self.dm.cur_start_time, e_time=self.dm.cur_end_time)
 
 
 class DataManager():
@@ -49,39 +69,117 @@ class DataManager():
         self.x_columns = None
         self.y_columns = None
         self.plot_data = None
+        self.current_session_key = None
+        self.cur_start_time = None
+        self.cur_end_time = None
+        self.cur_plot_df = None
+        self.sessions = dict()
+
         if self.data is not None:
             self.inititialise_data()
-        # self.data.plot(subplots=True, figsize=(6, 6))
-        # plt.legend(loc='best')
 
     def inititialise_data(self):
         if ('Date (D/M/Y)' in self.data.columns) & ('Time (H:M:S)' in self.data.columns):
             # Filter out null dates
             null_dates = self.data['Date (D/M/Y)'] == '0/0/0'
             self.data = self.data[~null_dates]
+            self.data = self.data.dropna()
+            self.data = self.data.reset_index()
 
             # Concatenate date and time into single column
             datetime_str = self.data['Date (D/M/Y)'] + 'T' + self.data['Time (H:M:S)']
             date_times = pd.to_datetime(datetime_str, format='%d/%m/%YT%H:%M:%S')
-            self.data['date_time'] = pd.Series(date_times)
-
-            self.data.index = self.data['date_time']
-
-            self.x_columns = [self.data.index.name] + self.data.columns.tolist()
+            date_times = pd.Series(date_times)
+            del self.data['Date (D/M/Y)']
+            del self.data['Time (H:M:S)']
+            self.x_columns = ['date_time'] + self.data.columns.tolist()
             self.y_columns = self.data.columns.tolist()
+            self.data['date_time'] = date_times
+            # Create dictionary for which each entry is a session
+            self.split_on_id()
+            session_keys = self.get_sorted_session_keys()
+            # Set current session
+            self.current_session_key = session_keys[0]
+            self.cur_plot_df = self.sessions[self.current_session_key]
+            self.cur_start_time = self.cur_plot_df['date_time'][0]
+            self.cur_end_time = self.cur_plot_df['date_time'][len(self.cur_plot_df)-1]
+            # Create plot data dictionary
             self.plot_data = dict()
-            self.plot_data['x_name'] = self.data.index.name
-            self.plot_data['x_data'] = self.data.index.values
+            self.plot_data['x_name'] = 'date_time'
+            self.plot_data['x_data'] = self.cur_plot_df['date_time'].values
             self.plot_data['y_name'] = self.y_columns[1]
-            self.plot_data['y_data'] = self.data[self.plot_data['y_name']].values
-
-    def update_y_plot_data(self, new_value):
-        self.plot_data['y_name'] = new_value
-        self.plot_data['y_data'] = self.data[new_value].values
+            self.plot_data['y_data'] = self.cur_plot_df[self.plot_data['y_name']].values
 
     def update_x_plot_data(self, new_value):
         self.plot_data['x_name'] = new_value
-        self.plot_data['x_data'] = self.data[new_value].values
+        self.plot_data['x_data'] = self.cur_plot_df[new_value].values
+
+    def update_y_plot_data(self, new_value):
+        self.plot_data['y_name'] = new_value
+        self.plot_data['y_data'] = self.cur_plot_df[new_value].values
+
+    def update_session_plot_data(self, new_session_key):
+        self.current_session_key = new_session_key
+        self.cur_plot_df = self.sessions[self.current_session_key]
+        self.plot_data['x_data'] = self.cur_plot_df[self.plot_data['x_name']].values
+        self.plot_data['y_data'] = self.cur_plot_df[self.plot_data['y_name']].values
+        start_time = self.cur_plot_df['date_time'][0]
+        self.cur_start_time = DataManager.PdTimestamp2Datetime(start_time).time()
+        end_time = self.cur_plot_df['date_time'][len(self.cur_plot_df) - 1]
+        self.cur_end_time = DataManager.PdTimestamp2Datetime(end_time).time()
+
+    # Splits data into different sessions if there is a gap of >10m between points
+    def split_on_time(self):
+        self.data['tdelta'] = (self.data['date_time'] - self.data['date_time'].shift()).fillna(0)
+        gap_indexes = self.data[self.data['tdelta'] > pd.Timedelta('10 m')].index.tolist()
+        if gap_indexes:
+            start_idx = 0
+            for gap_index in gap_indexes:
+                df_session = self.data[start_idx: gap_index-2]
+                self.sessions.append(df_session)
+                start_idx = gap_index
+
+    def split_on_id(self):
+        gap_indexes = self.data[self.data['Session Index'] == 0].index.tolist()
+        if len(gap_indexes) == 1:
+            self.sessions[self.data['date_time'][0]] = self.data
+        else:
+            for i in range(len(gap_indexes) - 1):
+                df_session = self.data[gap_indexes[i]: gap_indexes[i+1]]
+                df_session = df_session.reset_index()
+                self.sessions[df_session['date_time'][0]] = df_session
+            df_session = self.data[gap_indexes[len(gap_indexes)-1]:]
+            df_session = df_session.reset_index()
+            self.sessions[df_session['date_time'][0]] = df_session
+
+    def get_sorted_session_keys(self):
+        if self.sessions:
+            session_keys = []
+            for key in self.sessions:
+                session_keys.append(key)
+            return sorted(session_keys)
+        else:
+            return None
+
+    def apply_time_filter(self, start_time, end_time):
+        import datetime as dt
+        self.cur_start_time = start_time
+        self.cur_end_time = end_time
+        date = self.sessions[self.current_session_key]['date_time'][0].date()
+        start_time = pd.to_datetime(dt.datetime.combine(date, start_time))
+        end_time = pd.to_datetime(dt.datetime.combine(date, end_time))
+        current_session = self.sessions[self.current_session_key]
+        self.cur_plot_df = current_session[(current_session['date_time'] > start_time) &
+                                           (current_session['date_time'] < end_time)]
+        self.plot_data['x_data'] = self.cur_plot_df[self.plot_data['x_name']].values
+        self.plot_data['y_data'] = self.cur_plot_df[self.plot_data['y_name']].values
+
+
+    @staticmethod
+    def PdTimestamp2Datetime(PdTimestamp):
+        import datetime
+        time_str = str(PdTimestamp)
+        return datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
 
 
 if __name__ == "__main__":
